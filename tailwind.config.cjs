88c7ff4d9032bfc94c6e72431b2a08c079c03c7c
emileyr <emileyrochek@gmail.com>
2022-07-25 09:46:47 -0400

    added a bit of info/flair

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,33 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "fade-in-1": "fadeIn1 4s ease-in",
+        "fade-in-2": "fadeIn2 4s ease-in",
+        "fade-in-3": "fadeIn3 4s ease-in",
+      },
+      keyframes: (theme) => ({
+        fadeIn1: {
+          "0%": { opacity: 0 },
+          "25%": { opacity: 0 },
+          "37.5%": { opacity: 1 },
+          "100%": { opacity: 1 },
+        },
+        fadeIn2: {
+          "0%": { opacity: 0 },
+          "62.5%": { opacity: 0 },
+          "75%": { opacity: 1 },
+          "100%": { opacity: 1 },
+        },
+        fadeIn3: {
+          "0%": { opacity: 0 },
+          "75%": { opacity: 0 },
+          "87.5%": { opacity: 1 },
+          "100%": { opacity: 1 },
+        },
+      }),
+    },
   },
   plugins: [],
 };
